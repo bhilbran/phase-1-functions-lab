c198b57bebf0c7088a3d0b6135bd6e88e5bd0ca3
brooke <bhilbran@asu.edu>
2023-07-30 21:00:14 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    return Math.abs(blockNumber - 42);
+}
+
+function distanceFromHqInFeet (feetNumber) {
+    return distanceFromHqInBlocks(feetNumber)*264;
+}
+
+function distanceTravelledInFeet(a,b){
+    if (b>a){
+      return((b-a)*264);
+    }
+    else
+      return((a-b)*264);
+  }
+  
+  function calculatesFarePrice(start,destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance<= 400){
+      return 0;
+    }
+    if (distance>=400 && distance<=2000){
+      return (distance-400)* 0.02;
+    }
+    else if(distance>=2000&& distance<=2500){
+      return 25;
+    }
+    else if (distance > 2500){
+      return "cannot travel that far";
+    }
+  }
